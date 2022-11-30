3bdb98d6ac981fa0931d4b8d5898a52cc54187dc
Simon Wendel <mail@simonwendel.se>
2022-11-30 19:20:09 +0000

    feat: main script compiles binaries if missing

--- ickie_numerals.sh
+++ ickie_numerals.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-BIN_DIR=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)/bin
-$BIN_DIR/num2ick "$1" | $BIN_DIR/ick2roman | xargs
+ROOT_DIR=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
+BIN_DIR=$ROOT_DIR/bin
+N2I=$BIN_DIR/num2ick
+I2R=$BIN_DIR/ick2roman
+
+if [ ! -f "$N2I" ] || [ ! -f "$I2R" ]; then
+    $ROOT_DIR/build.sh
+fi
+
+$N2I "$1" | $I2R | xargs
